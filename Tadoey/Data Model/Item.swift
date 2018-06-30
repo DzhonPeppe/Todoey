26aaa76e99c914d3ca18e2222754c63f6f45507c
Dzhon <johnpeppe@gmail.com>
2018-06-30 07:26:17 +0300

    Convert to MVC

---
+++ Tadoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Tadoey
+//
+//  Created by John Peppe on 6/29/18.
+//  Copyright © 2018 John Peppe. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
